e1f44e0181028290452108b2b59aea47d33cb0cd
HieuTrungCao <21021490@vnu.edu.vn>
2024-05-16 10:05:10 +0700

    render vietnam map

---
+++ components/Vietnam_Map.js
@@ -0,0 +1,48 @@
+import React, { useRef } from 'react'
+import { useGLTF } from '@react-three/drei'
+
+export default function Model(props) {
+  const { nodes, materials } = useGLTF(require("../assets/Vietnam_Map.glb"))
+  return (
+    <group {...props} dispose={null} 
+      scale={1.25} 
+      position={[1, -6, 0]}
+      rotation={[1.57, 0, 0]}  
+    >
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes['G-__555591'].geometry}
+        material={materials['[0046_Gold].003']}
+        position={[3.43, 0, 0.47]}
+        scale={[7, 3.5, 7]}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes['G-__555564001'].geometry}
+        material={materials['[0046_Gold].004']}
+        position={[3.04, 0, 7.18]}
+        scale={[7, 3.5, 7]}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.VN_map.geometry}
+        material={materials.map}
+        position={[-0.028, 0, -0.077]}
+        scale={15}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.island.geometry}
+        material={materials.map}
+        position={[-2.691, 0, 5.532]}
+        scale={15}
+      />
+    </group>
+  )
+}
+
+useGLTF.preload(require("../assets/Vietnam_Map.glb"))
