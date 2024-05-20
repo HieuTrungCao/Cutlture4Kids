6b482a37f06a15fca1d412886b01155704d3b09a
HieuTrungCao <21021490@vnu.edu.vn>
2024-05-20 15:15:31 +0700

    fix name model

--- src/Constant.js
+++ src/Constant.js
@@ -1,7 +1,7 @@
 const CONST = {
-    ONE_PILLAR_PAGODA_NAME: "Chùa một cột",
-    HO_CITADEL_NAME: "Thành nhà Hồ",
-    FLAGPOLE_NAME: "Cột cờ Trường Sa"
+    ONE_PILLAR_PAGODA_NAME_VI: "Chùa một cột",
+    HO_CITADEL_NAME_VI: "Thành nhà Hồ",
+    FLAGPOLE_NAME_VI: "Cột cờ Trường Sa"
 }
 
 export default CONST;
