2571f69de943fbba102259e8e326ecc1e4ec995b
HieuTrungCao <21021490@vnu.edu.vn>
2024-05-20 21:06:37 +0700

    init info page

--- src/Constant.js
+++ src/Constant.js
@@ -2,7 +2,7 @@ const CONST = {
     ONE_PILLAR_PAGODA_NAME_VI: "Chùa một cột",
     HO_CITADEL_NAME_VI: "Thành nhà Hồ",
     FLAGPOLE_NAME_VI: "Cột cờ Trường Sa",
-    TAY_HOUSE_NAME_VI: "Nhà người Tày",
+    TAY_HOUSE_NAME_VI: "Nhà sàn người Tày",
     ONE_PILLAR_PAGODA_NAME: "OnePillarPagoda",
     HO_CITADEL_NAME: "HoCitadel",
     FLAGPOLE_NAME: "Flagpole",
